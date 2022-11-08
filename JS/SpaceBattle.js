209bebe5dc12fee5d89de186b8860b43cde7067c
Natalia Kostka <natalia7kostka@gmail.com>
2022-11-08 11:38:32 -0700

    adding first attack

--- JS/SpaceBattle.js
+++ JS/SpaceBattle.js
@@ -1,17 +1,12 @@
-//The Spaceship
+//THE SPACESHIP
 const spaceShip = {
     name: "USS HelloWorld",
     startingHull: 20,
     firePower: 5,
-    randomAccuracyRange(min, max){
-        return Math.floor(Math.random() * (max - min + 1)) + min
-    },
+    accuracy: 0.7,
     attackEnemy(){
-        if (randomAccuracyRange === 0){
-            console.log("Your attack missed!")
-        } else {
-            console.log("The Enemy has been hit!")
-        };
+        const successfulAttack = Math.random() > 0.7 ? "The Enemy has been hit!" : "Your attack missed!";
+        console.log(successfulAttack);
     },
     retreat(){
         const randomBool = Math.random() > 0.5 ? "Retreat Successful! Enemy evaded!" : "The enemy blocked your escape! Keep fighting.";
@@ -19,7 +14,7 @@ const spaceShip = {
     }
 };
 
-//The Enemy Ship
+//THE ENEMY SHIP
 const alienShip = {
         name: "Enemy Ship",
         randomHullRange(min, max){
@@ -40,26 +35,14 @@ const alienShip = {
         }
     };
 
-//START THE GAME
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     const startGame = function() {
-//         for(let i = 0; i < plot.length; i++){
-//             return confirm(plot[i]);
-//         };
-//     startGame();
-    
-//   document.querySelector('#game-play-btn').addEventListener('click', (evt) => {
-//     startGame()
-//   })
-// }});
 
 //THE STORY
 let plot = [
     `You are the captain of USS HelloWorld, traveling the beautiful galaxy of Javaeous Scriptius, when suddenly, you spot an enemy alien ship rapidly approaching!`,
     `Your ship’s hull is equipped with intergalactic armor made of strong moon rock, giving you a starting health status of 20 points.`,
     `Your ship also has powerful lasers made of stardust, damaging your enemies by 5 points with each hit.`,
-    `However, the spaceship’s mechanic has been on sabbatical for the last light-year and the ship’s lasers can be unreliable, only hitting the enemy ship 50% of the time.`,
-    `...... The Enemy is approaching!!! Ready your aim and fire! Open your console to keep track of your stats!`
+    `However, the spaceship’s mechanic has been on sabbatical for the last light-year and the ship’s lasers can be unreliable, only hitting the enemy ship 70% of the time.`,
+    `...... The Enemy is approaching!!! You have the advantage of attacking first - ready your aim and fire! Open your console to keep track of your stats!`
 ];
 
 //START THE GAME
@@ -70,8 +53,13 @@ function Start(){
     alert(plot[i]);
 }};
 
-startGame.addEventListener('click', Start)
+startGame.addEventListener('click', Start);
 
+//THE FIRST ATTACK
+function firstAttack(){
+    console.log(spaceShip.attackEnemy());
+};
 
+firstAttack();
 
 
